18ad0cd6d657c59edec08c21311b1606231dfe2c
tomney <gregory.tomney@gmail.com>
2019-04-26 09:07:58 -0400

    call URL asynchronously and append results from diff channels

--- main.go
+++ main.go
@@ -30,7 +30,6 @@ func main() {
 
 func getURLInput(r *bufio.Reader) (string, error) {
 	fmt.Print("Please enter the url you would like to download from then hit enter: \n")
-
 	delimiter := '\n'
 	url, err := r.ReadString(byte(delimiter))
 	if err != nil {
@@ -44,70 +43,55 @@ func getURLInput(r *bufio.Reader) (string, error) {
 func getURLResponseBody(url string) ([]byte, error) {
 	client := &http.Client{}
 	var wg sync.WaitGroup
-	wg.Add(1)
-	c1 := make(chan []byte, 1000000)
-	// c2 := make(chan []byte)
-	// c3 := make(chan []byte)
-	// c4 := make(chan []byte)
-	// Come up with a system for getting errors as well
+	wg.Add(4)
+	c1 := make(chan []byte, 1)
+	c2 := make(chan []byte, 1)
+	c3 := make(chan []byte, 1)
+	c4 := make(chan []byte, 1)
+	// TODO: Come up with a system for getting errors as well
 	// errChan := make(chan error)
 	go func() {
 		defer wg.Done()
-		fmt.Printf("Getting first byteList")
 		responseBody, _ := getURLResponseBodyAsync(client, url, "bytes=0-999999")
-		fmt.Printf("And we are assigning the responseBody to a channel")
 		c1 <- responseBody
 	}()
-	// go func() {
-	// 	defer wg.Done()
-	// 	fmt.Printf("Getting second byteList")
-	// 	responseBody, _ := getURLResponseBodyAsync(client, url, "bytes=1000000-1999999")
-	// 	c2 <- responseBody
-	// 	fmt.Printf("Got second byteList")
-	// }()
-	// go func() {
-	// 	defer wg.Done()
-	// 	fmt.Printf("Getting third byteList")
-	// 	responseBody, _ := getURLResponseBodyAsync(client, url, "bytes=2000000-2999999")
-	// 	c3 <- responseBody
-	// 	fmt.Printf("Got third byteList")
-	// }()
-	// go func() {
-	// 	defer wg.Done()
-	// 	fmt.Printf("Getting fourth byteList")
-	// 	responseBody, _ := getURLResponseBodyAsync(client, url, "bytes=3000000-3999999")
-	// 	c4 <- responseBody
-	// 	fmt.Printf("Got fourth byteList")
-	// }()
+	go func() {
+		defer wg.Done()
+		responseBody, _ := getURLResponseBodyAsync(client, url, "bytes=1000000-1999999")
+		c2 <- responseBody
+	}()
+	go func() {
+		defer wg.Done()
+		responseBody, _ := getURLResponseBodyAsync(client, url, "bytes=2000000-2999999")
+		c3 <- responseBody
+	}()
+	go func() {
+		defer wg.Done()
+		responseBody, _ := getURLResponseBodyAsync(client, url, "bytes=3000000-3999999")
+		c4 <- responseBody
+	}()
 	wg.Wait()
 	close(c1)
+	close(c2)
+	close(c3)
+	close(c4)
 
-	// for err := range errChan {
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// }
-
-	var body []byte
+	body := make([]byte, 400000, 4000000)
 	for byteList := range c1 {
-		fmt.Printf("Appending first byteList")
 		body = append(body, byteList...)
 	}
 
-	// for byteList := range c2 {
-	// 	fmt.Printf("Appending second byteList")
-	// 	body = append(body, byteList...)
-	// }
+	for byteList := range c2 {
+		body = append(body, byteList...)
+	}
 
-	// for byteList := range c3 {
-	// 	fmt.Printf("Appending third byteList")
-	// 	body = append(body, byteList...)
-	// }
+	for byteList := range c3 {
+		body = append(body, byteList...)
+	}
 
-	// for byteList := range c4 {
-	// 	fmt.Printf("Appending fourth byteList")
-	// 	body = append(body, byteList...)
-	// }
+	for byteList := range c4 {
+		body = append(body, byteList...)
+	}
 
 	return body, nil
 }
